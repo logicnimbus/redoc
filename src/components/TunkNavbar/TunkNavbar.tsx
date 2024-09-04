9a950e5e2586842e5f1c7bf675f6004d69d12385
daman-kinra <daman.kinra@gmail.com>
2024-09-04 22:59:17 +0530

    feat: added all redirections

--- src/components/TunkNavbar/TunkNavbar.tsx
+++ src/components/TunkNavbar/TunkNavbar.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import * as React from 'react';
 import styled from '../../styled-components';
-import { Dropdown, MenuProps } from 'antd';
+import { Button, ConfigProvider, Drawer, Dropdown, MenuProps } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 
 const tunkBaseUrl = 'https://tunk.ai';
 
@@ -11,7 +12,7 @@ const NavbarWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(12, minmax(0, 1fr));
   gap: 1rem;
-  @media (max-width: 760px) {
+  @media (max-width: 1024px) {
     padding: 1rem;
   }
 `;
@@ -28,9 +29,18 @@ const List = styled.ul`
   align-items: center;
   justify-content: space-between;
   list-style: none;
-  @media (max-width: 760px) {
+  @media (max-width: 1024px) {
     display: none;
   }
+`;
+const VerticalList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  grid-column: span 6 / span 6;
+  gap: 1.5rem;
+  align-items: center;
+  justify-content: space-between;
+  list-style: none;
 `;
 const ListItem = styled.li`
   cursor: pointer;
@@ -46,7 +56,7 @@ const Logo = styled.div`
   grid-column: span 2 / span 2;
   display: flex;
   align-items: center;
-  @media (max-width: 760px) {
+  @media (max-width: 1024px) {
     grid-column: span 4 / span 4;
   }
 `;
@@ -62,12 +72,30 @@ const ProductListWrapper = styled.div`
   align-items: center;
   cursor: pointer;
 `;
-
+const DashboardButtonWrapper = styled.div`
+  display: flex;
+  grid-column: span 4 / span 4;
+  justify-content: end;
+  align-items: center;
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+const MenuIconWrapper = styled.div`
+  display: none;
+  @media (max-width: 1024px) {
+    display: flex;
+    grid-column: span 8 / span 8;
+    width: 100%;
+    justify-content: flex-end;
+    align-items: center;
+  }
+`;
 const items: MenuProps['items'] = [
   {
     key: '1',
     label: (
-      <Anchor href={tunkBaseUrl}>
+      <Anchor href={`${tunkBaseUrl}/dashboard`}>
         <div className="px-2 py-2 text-base text-primaryDark">AI Transcription</div>
       </Anchor>
     ),
@@ -75,7 +103,7 @@ const items: MenuProps['items'] = [
   {
     key: '2',
     label: (
-      <Anchor href={tunkBaseUrl}>
+      <Anchor href={`${tunkBaseUrl}/human-transcription`}>
         <div className="px-2 py-2 text-base text-primaryDark">Human Transcription</div>
       </Anchor>
     ),
@@ -83,7 +111,7 @@ const items: MenuProps['items'] = [
   {
     key: '3',
     label: (
-      <Anchor href={tunkBaseUrl}>
+      <Anchor href={`${tunkBaseUrl}/anya`}>
         <div className="px-2 py-2 text-base text-primaryDark">Anya</div>
       </Anchor>
     ),
@@ -91,7 +119,7 @@ const items: MenuProps['items'] = [
   {
     key: '4',
     label: (
-      <Anchor href={tunkBaseUrl}>
+      <Anchor href={`${tunkBaseUrl}/earnings-call`}>
         <div className="px-2 py-2 text-base text-primaryDark">Earnings Call</div>
       </Anchor>
     ),
@@ -99,47 +127,132 @@ const items: MenuProps['items'] = [
 ];
 
 function TunkNavbar() {
+  const [showDrawer, setShowDrawer] = React.useState(false);
   return (
-    <NavbarWrapper>
-      <Logo>
-        <Anchor href={tunkBaseUrl}>
-          <Image src="https://d3tyi07qgwwvqk.cloudfront.net/tunk-logo.svg" />
-        </Anchor>
-      </Logo>
-      <List>
-        <Anchor href={tunkBaseUrl}>
-          <ListItem>Home</ListItem>
-        </Anchor>
-        <Dropdown menu={{ items }}>
-          <ProductListWrapper>
-            <ListItem>Products</ListItem>
-            <Icon
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 512 512"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"></path>
-            </Icon>
-          </ProductListWrapper>
-        </Dropdown>
-        <Anchor href={`${tunkBaseUrl}/about-us`}>
-          <ListItem>About Us</ListItem>
-        </Anchor>
-        <Anchor href={`${tunkBaseUrl}/pricing`}>
-          <ListItem>Pricing</ListItem>
-        </Anchor>
-        <Anchor href="https://www.blog.tunk.ai">
-          <ListItem>Blog</ListItem>
-        </Anchor>
-        <Anchor href={`${tunkBaseUrl}/contact-us`}>
-          <ListItem>Contact Us</ListItem>
-        </Anchor>
-      </List>
-    </NavbarWrapper>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#0f3057',
+          fontSize: 12,
+        },
+        components: {
+          Button: {
+            primaryShadow: 'none',
+            defaultBorderColor: '#0f3057',
+            defaultColor: '#0f3057',
+          },
+        },
+      }}
+    >
+      <NavbarWrapper>
+        <Logo>
+          <Anchor href={tunkBaseUrl}>
+            <Image src="https://d3tyi07qgwwvqk.cloudfront.net/tunk-logo.svg" />
+          </Anchor>
+        </Logo>
+        <List>
+          <Anchor href={tunkBaseUrl}>
+            <ListItem>Home</ListItem>
+          </Anchor>
+          <Dropdown menu={{ items }}>
+            <ProductListWrapper>
+              <ListItem>Products</ListItem>
+              <Icon
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth="0"
+                viewBox="0 0 512 512"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M256 294.1L383 167c9.4-9.4 24.6-9.4 33.9 0s9.3 24.6 0 34L273 345c-9.1 9.1-23.7 9.3-33.1.7L95 201.1c-4.7-4.7-7-10.9-7-17s2.3-12.3 7-17c9.4-9.4 24.6-9.4 33.9 0l127.1 127z"></path>
+              </Icon>
+            </ProductListWrapper>
+          </Dropdown>
+          <Anchor href={`${tunkBaseUrl}/about-us`}>
+            <ListItem>About Us</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/pricing`}>
+            <ListItem>Pricing</ListItem>
+          </Anchor>
+          <Anchor href="https://www.blog.tunk.ai">
+            <ListItem>Blog</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/contact-us`}>
+            <ListItem>Contact Us</ListItem>
+          </Anchor>
+        </List>
+        <DashboardButtonWrapper>
+          <Button
+            type="primary"
+            onClick={() => window.open(`${tunkBaseUrl}/dashboard`)}
+            style={{
+              border: '3px solid #0274be',
+              borderRadius: '.125rem',
+              padding: '0.75rem 2.5rem',
+              height: '3rem',
+            }}
+          >
+            Go To Dashboard
+          </Button>
+        </DashboardButtonWrapper>
+        <MenuIconWrapper>
+          <MenuOutlined
+            onClick={() => setShowDrawer(true)}
+            style={{
+              fontSize: '1.25rem',
+              cursor: 'pointer',
+            }}
+          />
+        </MenuIconWrapper>
+      </NavbarWrapper>
+      <Drawer open={showDrawer} onClose={() => setShowDrawer(false)}>
+        <VerticalList>
+          <Anchor href={tunkBaseUrl}>
+            <ListItem>Home</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/dashboard`}>
+            <ListItem className="px-2 py-2 text-base text-primaryDark">AI Transcription</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/human-transcription`}>
+            <ListItem className="px-2 py-2 text-base text-primaryDark">
+              Human Transcription
+            </ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/anya`}>
+            <ListItem className="px-2 py-2 text-base text-primaryDark">Anya</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/earnings-call`}>
+            <ListItem className="px-2 py-2 text-base text-primaryDark">Earnings Call</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/about-us`}>
+            <ListItem>About Us</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/pricing`}>
+            <ListItem>Pricing</ListItem>
+          </Anchor>
+          <Anchor href="https://www.blog.tunk.ai">
+            <ListItem>Blog</ListItem>
+          </Anchor>
+          <Anchor href={`${tunkBaseUrl}/contact-us`}>
+            <ListItem>Contact Us</ListItem>
+          </Anchor>
+          <Button
+            type="primary"
+            onClick={() => window.open(`${tunkBaseUrl}/dashboard`)}
+            style={{
+              border: '3px solid #0274be',
+              borderRadius: '.125rem',
+              padding: '0.75rem 2.5rem',
+              height: '3rem',
+            }}
+          >
+            Go To Dashboard
+          </Button>
+        </VerticalList>
+      </Drawer>
+    </ConfigProvider>
   );
 }
 
